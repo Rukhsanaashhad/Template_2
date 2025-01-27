0ee6293160d2b15843d42dc2cee173ad2f3510e1
Ashhad <maxtraders712@gmail.com>
2025-01-27 16:09:47 +0500

    vercel error resolve

--- components/ImageView.tsx
+++ components/ImageView.tsx
@@ -1,16 +1,14 @@
 "use client";
-import {
-  internalGroqTypeReferenceTo,
-  SanityImageCrop,
-  SanityImageHotspot,
-} from "@/sanity.types";
-import React, { useState } from "react";
-import { motion, AnimatePresence } from "motion/react";
-import { urlFor } from "@/sanity/lib/image";
-import Image from "next/image";
+
+import { internalGroqTypeReferenceTo, SanityImageCrop, SanityImageHotspot } from '@/sanity.types';
+import React, { useState } from 'react'
+import {motion,AnimatePresence} from 'motion/react';  
+import Image from 'next/image';
+import { urlFor } from '@/sanity/lib/image';
+
 
 interface Props {
-  images?: Array<{
+  image?: Array<{
     asset?: {
       _ref: string;
       _type: "reference";
@@ -23,48 +21,24 @@ interface Props {
     _key: string;
   }>;
 }
-const ImageView = ({ images = [] }: Props) => {
-  const [active, setActive] = useState(images[0]);
+
+const ImageView = ({ image = [] }: Props) => {  
+    const [active, setActive]= useState(image)  
   return (
     <div className="w-full md:w-1/2 space-y-2 md:space-y-4">
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={active?._key}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.5 }}
-          className="w-full max-h-[550px] min-h-[450px] border border-darkColor/10 rounded-md group overflow-hidden"
-        >
-          <Image
-            src={urlFor(active).url()}
-            alt="productImage"
+     <AnimatePresence mode='wait'>
+        <motion.div className="w-full max-h-[550px] min-h-[450px] border border-darkColor/10 rounded-md group overflow-hidden">
+            <Image src={urlFor(active).url()} alt="product Image"
             width={700}
             height={700}
             priority
-            className="w-full h-96 max-h-[550px] min-h-[500px] object-contain group-hover:scale-110 hoverEffect rounded-md"
-          />
-        </motion.div>
-      </AnimatePresence>
-      <div className="grid grid-cols-6 gap-2 h-20 md:h-28">
-        {images?.map((image) => (
-          <button
-            onClick={() => setActive(image)}
-            key={image?._key}
-            className={`border rounded-md overflow-hidden ${active?._key === image?._key ? "ring-1 ring-darkColor" : ""}`}
-          >
-            <Image
-              src={urlFor(image).url()}
-              alt="productImage"
-              width={100}
-              height={100}
-              className="w-full h-auto object-contain"
+            className=" bg-gray-300 max-h-[550px] min-h-[500px] object-contain group-hover:scale-110 hoverEffect rounded-md"
             />
-          </button>
-        ))}
-      </div>
+        </motion.div>
+     </AnimatePresence>                  
+    
     </div>
-  );
-};
+  )
+}
 
-export default ImageView;
+export default ImageView
