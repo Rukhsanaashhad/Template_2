d189f0c9fc8695f1fc302be8fd192c0a8e89c779
Ashhad <maxtraders712@gmail.com>
2025-02-01 17:54:43 +0500

    vercel error resolve

--- components/ImageView.tsx
+++ components/ImageView.tsx
@@ -7,8 +7,7 @@ import { urlFor } from '@/sanity/lib/image'; // Adjust as necessary
 
 // Define types for the props  
 type ImageViewProps = {  
-  image: string  
-     // Specify that url is a string  
+  image:any    // Specify that url is a string  
 };  
 
 const ImageView: React.FC<ImageViewProps> = ({ image }) => {  
